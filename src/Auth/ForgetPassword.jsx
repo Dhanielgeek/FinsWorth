d3cf9f3e9159cd58fe2e6cb853a43155b32bae59
Dhanielgeek <dhanielknight46@gmail.com>
2024-04-05 14:08:52 +0100

    newww

---
+++ src/Auth/ForgetPassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ForgetPassword = () => {
+  return (
+    <div className=' w-[100%] h-[100vh] bg-pink-500'>ForgetPassword</div>
+  )
+}
+
+export default ForgetPassword
